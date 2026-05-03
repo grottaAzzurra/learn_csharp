7eb9dcd32d02886063162bbb7a13804e4d23847b
grottaAzzurra <125438751+grottaAzzurra@users.noreply.github.com>
2026-05-03 14:45:38 -0700

    module 1 and 2

---
+++ store_and_retrieve_data/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello"); // String literal
+Console.WriteLine('b'); // Character literal
+Console.WriteLine(123); // Integer literal
+Console.WriteLine(0.25F); // Float literal
+Console.WriteLine(2.625); // Default double literal
+Console.WriteLine(12.39816M); // Decimal literal
+
+Console.WriteLine(true); // Boolean literal
+Console.WriteLine(false); // Boolean literal
