626f7e716adebead6cb002548c5e941fc425429e
grottaAzzurra <125438751+grottaAzzurra@users.noreply.github.com>
2026-05-03 20:21:30 -0700

    escaping characters

---
+++ basic_string_formatting/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello\nWorld!");
+Console.WriteLine("Hello\tWorld!");
+Console.WriteLine("When you want to include a \'\"\' quote in a string, you need a backslash to escape it");
+
+Console.WriteLine("Generating invoice for customer \"Haptifactory\" ... \n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.Write("\nOutputDirectory:\t");
