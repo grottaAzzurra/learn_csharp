14301dbc2f149b282b25f275305b936a478416ee
grottaAzzurra <125438751+grottaAzzurra@users.noreply.github.com>
2026-05-04 14:46:24 -0700

    formatting with special characters

--- basic_string_formatting/Program.cs
+++ basic_string_formatting/Program.cs
@@ -2,8 +2,14 @@
 Console.WriteLine("Invoice: 1021\t\tComplete!");
 Console.WriteLine("Invoice: 1022\t\tComplete!");
 Console.Write("\nOutput Directory:\t");
+// The @ symbol is a directive. It will create a verbatim string that does not require any other escaping.
 Console.WriteLine(@"c:\invoices");
-Console.WriteLine(" ");
 
 // The following line prints Hello World in Japanese
-Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
+// Console.WriteLine("\u3053\u3093\u306B\u3061\u306F World!");
+
+// To generate Japanese invoices:
+// Nihon no seikyū-sho o seisei suru ni wa:
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+// User command to run an application
+Console.WriteLine(@"c:\invoices\app.exe -j");
